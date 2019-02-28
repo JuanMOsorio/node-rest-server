2d47d76a467e64cdec69e8dbef9e6be8d5d31750
JuanMOsorio <juan24.martinez05@gmail.com>
2019-02-28 07:50:25 -0600

    Codigos de respuesta

--- server/server.js
+++ server/server.js
@@ -14,7 +14,14 @@ app.get('/user', (req, res) => {
 app.post('/user', (req, res) => {
 	let body = req.body;
 
-	res.json({ body });
+	if (body.name === undefined) {
+		res.status(400).json({
+			ok: false,
+			message: 'The name is required'
+		});
+	} else {	
+		res.json({ body });
+	}
 });
 
 app.put('/user/:id', (req, res) => {
