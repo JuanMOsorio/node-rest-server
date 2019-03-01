39ff1221e06897aca645f05ced421ea046685cf7
JuanMOsorio <juan24.martinez05@gmail.com>
2019-03-01 08:04:38 -0600

    Configuando rutas de usuario

---
+++ server/routes/user-routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/user', (req, res) => {
+	res.json('Get User');
+});
+
+app.post('/user', (req, res) => {
+	let body = req.body;
+
+	if (body.name === undefined) {
+		res.status(400).json({
+			ok: false,
+			message: 'The name is required'
+		});
+	} else {	
+		res.json({ body });
+	}
+});
+
+app.put('/user/:id', (req, res) => {
+	let id = req.params.id;
+
+	res.json({
+		id
+	});
+});
+
+app.delete('/user', (req, res) => {
+	res.json('Delete User');
+});
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -12,34 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/user', (req, res) => {
-	res.json('Get User');
-});
-
-app.post('/user', (req, res) => {
-	let body = req.body;
-
-	if (body.name === undefined) {
-		res.status(400).json({
-			ok: false,
-			message: 'The name is required'
-		});
-	} else {	
-		res.json({ body });
-	}
-});
-
-app.put('/user/:id', (req, res) => {
-	let id = req.params.id;
-
-	res.json({
-		id
-	});
-});
-
-app.delete('/user', (req, res) => {
-	res.json('Delete User');
-});
+// Usando Rutas.
+app.use(require('./routes/user-routes'));
 
 // Conexion con mongo db
 mongoose.connect('mongodb://localhost:27017/coffe', (err, res) => {
