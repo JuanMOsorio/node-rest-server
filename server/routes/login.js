10ed01aef463ad501d177a1b0b69d1c516d53b07
JuanMOsorio <juan24.martinez05@gmail.com>
2019-03-08 09:28:43 -0600

    Login de usuario

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,9 +7,39 @@ const app = express();
 
 // RUTAS PARA EL LOGIN.
 app.post('/login', (req, res) => {
-	res.json({
-		ok: true
-	})
+
+	// Obtener el body.
+	let body = req.body;
+
+	User.findOne({ email: body.email }, (err, userDB) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				err
+			});
+		}
+		if (!userDB) {
+			return res.status(400).json({
+				ok: false,
+				err: {
+					message: '(User)  password incorrectos!!'
+				}
+			});
+		}
+		if (!bcrypt.compareSync(body.password, userDB.password)) {
+			return res.status(400).json({
+				ok: false,
+				err: {
+					message: 'User (password) incorrectos!!'
+				}
+			});
+		}
+		res.json({
+			ok: true,
+			user: userDB,
+			token: '123'
+		});
+	});
 });
 
 module.exports = app;
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -45,7 +45,8 @@ app.post('/user', (req, res) => {
 		name: body.name,
 		email: body.email,
 		password: bcrypt.hashSync(body.password, 10),
-		role: body.role
+		role: body.role,
+		// img: body.img
 	});
 	
 
