8af3c1895fccb9ff6774deeebac06c7b37512fc7
Juan Martínez Osorio <juan24.martinez05@gmail.com>
2019-03-13 07:29:50 -0600

    Creando usuario de google con credenciales guradando en la base de datos con validaciones (Error aun no guarda la informacion de la imagen de usurio desde google)

--- server/routes/login.js
+++ server/routes/login.js
@@ -67,22 +67,104 @@ async function verify(token) {
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  console.log(payload.name);
-  console.log(payload.email);
-  console.log(payload.picture);
+  // const userid = payload['sub'];
+
+  // Obteniendo la información del payload.
+  return {
+  	name: payload.name,
+  	email: payload.email,
+  	img: payload.img,
+  	google: true
+  };
 }
 
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
 	// Obtener el token.
 	let token = req.body.idtoken;
 
 	// Lamando la funcion de verificar.
-	verify(token);
+	// Obteniendo la información del usuario
+	let googleUser = await verify(token)
+		.catch(err => {
+		 return	res.status(403).json({
+				ok: false,
+				err
+			});
+		});
 
-	res.json({
-		token
+
+	// Validaciones para el usuario de Google.
+	User.findOne({ email: googleUser.email }, (err, userDB) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				err
+			});
+		}
+
+		// Verificar si el usuario exite.
+		if (userDB) {
+			// Verficar si el usario se a autenticado con google.
+			if (userDB.google === false) {
+				return res.status(400).json({
+					ok: false,
+					err: {
+						message: 'Debe usar la autenticación normal!'
+					}
+				});
+			} else {
+				// Generando un token para el usuario.
+				let token = jwt.sign({
+					user: userDB
+				}, process.env.SEED, { expiresIn: process.env.EXPIRATION_TOKEN });
+
+				// Retornando la información del usuario.
+				return res.json({
+					ok: true,
+					user: userDB,
+					token
+				});
+			}
+		} else {
+			// Si el usario no existe en la base de datos.
+			// Se autentica por primera vez.
+
+			let user = new User();
+
+			user.name = googleUser.name;
+			user.email = googleUser.email;
+			user.img = googleUser.img;
+			user.google = true;
+			user.password = ':)';
+			
+			user.save((err, userDB) => {
+				// Si hay un error.
+				if (err) {
+					return res.status(500).json({
+						rok: false,
+						err
+					});
+				}
+
+				// Retornar la información del nuenvo usuario de la base de datos.
+
+				// Generando un token para el usuario.
+				let token = jwt.sign({
+					user: userDB
+				}, process.env.SEED, { expiresIn: process.env.EXPIRATION_TOKEN });
+
+				// Retornando la información del usuario.
+				return res.json({
+					ok: true,
+					user: userDB,
+					token
+				});
+
+			});
+		}
+
 	});
+
 });
 
 module.exports = app;
