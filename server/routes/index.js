b70a5cbbed949cecd876cd67c15826f50612d569
JuanMOsorio <juan24.martinez05@gmail.com>
2019-03-17 13:27:03 -0600

    CRUD de Productos finalizado

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const app = express();
 // Usando Rutas.
 app.use(require('./user-routes'));
 app.use(require('./category-routes'));
+app.use(require('./product-routes'));
 
 // Ruta de Login.
 app.use(require('./login'));
---
+++ server/routes/product-routes.js
@@ -0,0 +1,202 @@
+const express = require('express');
+const { checkToken } =require('../middlewares/autentication');
+
+const app = express();
+let Product = require('../models/products-model');
+
+//=============================
+// Obtener todos los productos.
+//=============================
+app.get('/product', checkToken, (req, res) => {
+	// trate todos los productos
+	// con el populate: user and category
+	// paginado
+
+	let start = req.query.start || 0;
+	start = Number(start);
+	let limit = req.query.limit || 5;
+	limit = Number(limit);
+
+	Product.find({ available: true })
+		.skip(start)
+		.limit(limit)
+		.populate('user', 'name email')
+		.populate('category', 'description')
+		.exec((err, productDB) => {
+
+			if (err) {
+				return res.status(500).json({
+					ok: false,
+					err
+				});
+			}
+
+			Product.count({ available: true }, (err, count) => {
+				res.json({
+					count,
+					ok: true,
+					product: productDB
+				});
+			});
+
+		});
+
+});
+
+//=============================
+// Obtener un producto por id.
+//=============================
+app.get('/product/:id', checkToken, (req, res) => {
+	// populate user category
+
+	let id = req.params.id;
+
+	Product.findById(id)
+		.populate('user', 'name email')
+		.populate('category', 'description')
+		.exec((err, productDB) => {
+
+			if (err) {
+				return res.status(500).json({
+					ok: false,
+					err
+				});
+			}
+
+			if (!productDB) {
+				return res.status(400).json({
+					ok: false,
+					err: {
+						message: 'El id es incorrecto'
+					}
+				});
+			}
+
+			res.json({
+				ok: true,
+				product: productDB
+			});
+
+	});
+});
+
+//=============================
+// Crear un nuevo producto.
+//=============================
+app.post('/product', checkToken, (req, res) => {
+	// grabar el user
+	// grabar una categoria del listado
+	let body = req.body;
+
+	let product = new Product({
+		name: body.name,
+		priceUni: body.priceUni,
+		description: body.description,
+		available: body.available,
+		category: body.category,
+		user: req.user._id
+	})
+
+	product.save((err, productDB) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				err
+			});
+		}
+
+		res.status(201).json({
+			ok: true,
+			product: productDB
+		});
+	});
+});
+
+//=============================
+// Actualizar un nuevo producto
+//=============================
+app.put('/product/:id', checkToken, (req, res) => {
+	let id = req.params.id;
+	let body = req.body;
+
+	Product.findById(id, (err, productDB) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				err
+			});
+		}
+
+		if (!productDB) {
+			return res.status(500).json({
+				ok: false,
+				err: {
+					message: 'El producto no existe'
+				}
+			});
+		}
+
+		productDB.name = body.name;
+		productDB.priceUni = body.priceUni;
+		productDB.description = body.description;
+		productDB.available = body.available;
+		productDB.category = body.category;
+
+		productDB.save((err, productUpdated) => {
+			if (err) {
+				return res.status(500).json({
+					ok: false,
+					err
+				});
+			}
+
+			res.json({
+				ok: true,
+				product: productUpdated
+			});
+		});
+
+	});
+
+});
+
+//=============================
+// Borrar un producto.
+//=============================
+app.delete('/product/:id', checkToken, (req, res) => {
+	// Cambiar el estado del producto
+	// para que no se visible.
+	let id = req.params.id;
+
+	let changeStatus = {
+		available: false
+	};
+
+	// User.findByIdAndRemove(id, (err, userDeleted) => {
+	Product.findByIdAndUpdate(id, changeStatus, { new: true },(err, productDeleted) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				err
+			});
+		}
+
+		if (!productDeleted) {
+			return res.status(400).json({
+				ok: false,
+				err:{
+					message: 'Product not find'
+				}
+			});	
+		}
+
+		res.json({
+			ok: true,
+			product: productDeleted,
+			message: 'Producto borrado!!'
+		});
+	});
+
+});
+
+module.exports = app;
